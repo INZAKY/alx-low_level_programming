aa7015c2ddacf561f4f86efacf77fd2447e5d401
Philip M Pawa <philip2pawa@gmail.com>
2023-08-10 11:46:41 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main(void){
 printf("Size of char: %zu byte(s)\n", sizeof(char));
 printf("Size of short: %zu byte(s)\n", sizeof(short));
 printf("Size of int: %zu byte(s)\n", sizeof(int));
@@ -9,6 +9,5 @@ printf("Size of long long: %zu byte(s)\n", sizeof(long long));
 printf("Size of float: %zu byte(s)\n", sizeof(float));
 printf("Size of double: %zu byte(s)\n", sizeof(double));
 printf("Size of long double: %zu byte(s)\n", sizeof(long double));
-
-return 0;
+return (0);
 }
