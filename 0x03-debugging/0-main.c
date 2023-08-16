23db9a4ad7e7ff28733d5a75ad1e54f6c284d9b3
Philip M Pawa <philip2pawa@gmail.com>
2023-08-16 14:56:40 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,9 +5,11 @@
 */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i;
+
+    i = 0;/* Change from i = 98; to i = 0; */
+    positive_or_negative(i);
+
+    return (0);
 }
 
