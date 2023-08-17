7a8ec971d2f36599e4a2cd3183004fe012bc5268
Philip M Pawa <philip2pawa@gmail.com>
2023-08-17 09:32:21 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,63 +2,39 @@
 #include <math.h>
 
 /**
- * is_prime - checks if a number is prime
- * @num: number to check
- * Return: 1 if prime, 0 if not
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-int is_prime(unsigned long num)
-{
-    if (num <= 1)
-        return 0;
-    if (num <= 3)
-        return 1;
-    if (num % 2 == 0 || num % 3 == 0)
-        return 0;
-
-    for (unsigned long i = 5; i * i <= num; i += 6)
-    {
-        if (num % i == 0 || num % (i + 2) == 0)
-            return 0;
-    }
-    return 1;
-}
-
-/**
- * largest_prime_factor - finds the largest prime factor of a number
- * @num: number to find the largest prime factor of
- * Return: largest prime factor
- */
-unsigned long largest_prime_factor(unsigned long num)
-{
-    unsigned long max_prime = 1;
-
-    while (num % 2 == 0)
-    {
-        max_prime = 2;
-        num /= 2;
-    }
-
-    for (unsigned long i = 3; i <= sqrt(num); i += 2)
-    {
-        while (num % i == 0)
-        {
-            max_prime = i;
-            num /= i;
-        }
-    }
-
-    if (num > 2 && is_prime(num))
-        max_prime = num;
-
-    return max_prime;
-}
-
 int main(void)
 {
-    unsigned long number = 612852475143;
-    unsigned long result = largest_prime_factor(number);
+	long int n;
+	long int max;
+	long int i;
 
-    printf("%lu\n", result);
+	n = 612852475143;
+	max = -1;
 
-    return 0;
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
 }
+
